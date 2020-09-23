3908c2c27284fab29924d0ad442e30042429c03e
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 22:08:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,13 +13,7 @@ class Song
     @@all 
   end
   
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+  
   
   def self.new_by_filename(filename)
     song = self.new
