053f88dcb74858ef2032d007190a6f5198ff4cd1
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 22:38:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,8 +13,10 @@ class MP3Importer
     end 
   
   def import
-    self.files.each {|file| song = Song.new_by_filename(file)}
+    self.files.each do |file| 
+      song = Song.new_by_filename(file)
     Artist.all << song.artist unless Artist.all.include?(song.artist)
+  end
   end
 
 end
