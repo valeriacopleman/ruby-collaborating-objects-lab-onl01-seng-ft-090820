6bf2577524a72eec8f8c9a6de411bbc534f83505
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 18:35:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,9 @@
 class MP3Importer
   
+  def initialize(path)
+    @path = path 
+  end
+  
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
