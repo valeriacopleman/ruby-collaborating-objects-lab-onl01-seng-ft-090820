122682e3173897007b4171f921438b9d9bcd5d94
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 22:18:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   def self.new_by_filename(filename)
     binding.pry
     split_name = filename.split(" - ")
-    self.new(split_name[1])
+    song = self.new(split_name[1])
   end
  
   
