44830c9109a7cc6b7a394b57bbbd97c7f1ce78fa
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 22:27:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    #binding.pry
+    binding.pry
     split_name = filename.split(" - ")
     song = self.new(split_name[1])
     artist = Artist.find_or_create_by_name(split_name[0])
